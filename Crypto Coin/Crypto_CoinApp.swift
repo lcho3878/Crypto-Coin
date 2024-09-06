3a5f81658e2c358cd53f8e1f82033069333d9e3b
lcho3878 <leech3878@naver.com>
2024-09-06 18:29:50 +0900

    Initial Commit

---
+++ Crypto Coin/Crypto_CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_CoinApp.swift
+//  Crypto Coin
+//
+//  Created by 이찬호 on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
